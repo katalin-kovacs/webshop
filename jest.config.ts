1d984ae42ce568b643ad1b78fb7a06b40a23a66f
Katalin Kovács <kovacs.katalin89@gmail.com>
2025-05-18 21:33:12 +0200

    remove jest, add karma and jasmine

--- jest.config.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testMatch: ['**/*.spec.ts'],
-  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
-};
